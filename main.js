ebcbaef99b5b2e8b2db5e06488e0edd882466c9c
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-11-30 13:52:12 -0600

    worked on js function to change display text.

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 "use strict";
+
+$(document).ready(function() {
+
+    const age = function() { $('#greeting').text('22!!!')};
+    const greeting = function() { $('#greeting').text('Happy Birthday Daniel!!')};
+
+    function changing() {
+        setTimeout(age, 2000);
+        setTimeout(greeting, 4000);
+    }
+
+    changing();
+
+    let startTimer;
+
+    for (let i = 0; i < 20; i++) {
+        startTimer = setInterval(function () {
+                changing();
+                console.log("hello");
+        }, 4000);
+    }
+
+
+
+
+
+});
