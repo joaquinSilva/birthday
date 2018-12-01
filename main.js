4f4c955a44f5d4176812c6b11979361ecaaa7f50
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-11-30 23:53:00 -0600

    fixed javascript on color change and also inner html changes. Made more style changes.

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
 "use strict";
 
+
+
 $(document).ready(function() {
 
-    const age = function() { $('#greeting').text('22!!!')};
-    const greeting = function() { $('#greeting').text('Happy Birthday Daniel!!')};
-
     function changing() {
-        setTimeout(age, 2000);
-        setTimeout(greeting, 4000);
+        let x = document.getElementById("greeting");
+        if (x.innerHTML === "Happy Birthday Daniel!!") {
+            x.innerHTML = "22!!!";
+        } else {
+            x.innerHTML = "Happy Birthday Daniel!!";
+        }
+    }
+    // https://stackoverflow.com/questions/17712247/why-innerhtml-dosent-work-inside-settimeout
+    function changeText(num) {
+        if (num > 0) {
+            changing();
+            console.log("is this working now?");
+            setTimeout(function(){changeText(num-1)}, 3000);
+        }
     }
 
-    changing();
-
-    let startTimer;
-
-    for (let i = 0; i < 20; i++) {
-        startTimer = setInterval(function () {
-                changing();
-                console.log("hello");
-        }, 4000);
-    }
+    changeText(60);
 
 
+    let cakeChange = document.getElementById('cake'); // cache the element
+    let greetingChange = document.getElementById('greeting'); // cache the element
+    let button = document.getElementById('button');
+    let tones = ['aqua','greenyellow','#c3f9ca', '#f9913c', '#f9283a']; // any amount can be added
+    let current = 0; // click counter
 
+    button.addEventListener('click', function() {
+
+        current++; // add one to click count
+        current %= tones.length; // if count is larger than length of array use modulus to return to beginning
+        cakeChange.style.color = tones[current];//change background style to current number in the array
+        greetingChange.style.color = tones[current];
+
+    });
 
 
 });
